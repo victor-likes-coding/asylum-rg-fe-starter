ac4aeb8e13ef7aaa4b882597198f29c74633974a
Victor Tran <victor.tran@tempestas.io>
2023-09-19 01:59:43 -0400

    rename fn name

--- src/components/common/logout-button.js
+++ src/components/common/logout-button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from 'react-bootstrap';
 
-export const LoginButton = () => {
+export const LogoutButton = () => {
   const { logout } = useAuth0();
   return (
     <Button variant="primary" className="btn-margin" onClick={() => logout()}>
