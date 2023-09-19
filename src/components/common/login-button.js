6112c4396783ce30bfb62b441b364e7041753d8b
Victor Tran <victor.tran@tempestas.io>
2023-09-19 02:13:05 -0400

    update components to be flat text

--- src/components/common/login-button.js
+++ src/components/common/login-button.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Button } from 'react-bootstrap';
 
-export const LoginButton = () => {
+export const LoginButton = ({ style }) => {
   const { loginWithRedirect } = useAuth0();
   return (
-    <Button
-      variant="primary"
-      className="btn-margin"
-      onClick={() => loginWithRedirect()}
-    >
+    <div className="pointer" style={style} onClick={() => loginWithRedirect()}>
       Log In
-    </Button>
+    </div>
   );
 };
--- src/components/common/logout-button.js
+++ src/components/common/logout-button.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from 'react-bootstrap';
 
-export const LogoutButton = () => {
+export const LogoutButton = ({ style }) => {
   const { logout } = useAuth0();
   return (
-    <Button variant="primary" className="btn-margin" onClick={() => logout()}>
+    <div style={style} onClick={() => logout()}>
       Log out
-    </Button>
+    </div>
   );
 };
