aaa923bfed91cce3c0d169481d407c96b30d5719
Victor Tran <victor.tran@tempestas.io>
2023-09-19 01:56:42 -0400

    create login and logout button

---
+++ src/components/common/logout-button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from 'react-bootstrap';
+
+export const LoginButton = () => {
+  const { logout } = useAuth0();
+  return (
+    <Button variant="primary" className="btn-margin" onClick={() => logout()}>
+      Log out
+    </Button>
+  );
+};
